4af19e1f1bee7110844b2cde66121b4afbbf45b8
Developedbypatels <developedbypatels@gmail.com>
2020-03-19 21:23:54 -0400

    res

--- resources/views/rooms/create.blade.php
+++ resources/views/rooms/create.blade.php
@@ -50,7 +50,7 @@
             <button type="submit" class="btn btn-primary">Add Reservation</button>
         </div>
         <div class="col-sm-9">
-            <a href="{{ route('bookings.index') }}" class="btn btn-secondary">Cancel</a>
+            <a href="{{ route('rooms.index') }}" class="btn btn-secondary">Cancel</a>
         </div>
     </div>
 </form>
