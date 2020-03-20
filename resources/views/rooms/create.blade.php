f1c1dd9f18da8e443df628abd773daeb19b96b12
Developedbypatels <developedbypatels@gmail.com>
2020-03-19 21:25:50 -0400

    res

--- resources/views/rooms/create.blade.php
+++ resources/views/rooms/create.blade.php
@@ -56,32 +56,4 @@
 </form>
 </div>
 
-<table class="table">
-    <thead>
-        <tr>
-            <th>
-                Room Number
-            </th>
-            <th>
-                Room Name
-            </th>
-            <th>
-                Room Description
-            </th>
-            <th>
-                Max Occupancy
-            </th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($rooms as $room)
-            <tr>
-                <td>{{ $room->room_number }}</td>
-                <td>{{ $room->room_name }}</td>
-                <td>{{ $room->room_desc }}</td>
-                <td>{{ $room->max_occupancy }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
 @endsection
