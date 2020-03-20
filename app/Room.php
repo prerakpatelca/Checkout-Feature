9edd0b4c92d14512b184b54a333e6efe99dad5d3
Developedbypatels <developedbypatels@gmail.com>
2020-03-20 01:33:37 -0400

    res

--- app/Room.php
+++ app/Room.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
-    //
+    protected $fillable = [
+        'room_number',
+        'room_name',
+        'room_desc',
+        'max_occupancy'
+    ];
 }
