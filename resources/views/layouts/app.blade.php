bd5cb5bb9d206b0789327a33126427feb2ade9d2
Developedbypatels <developedbypatels@gmail.com>
2020-03-21 00:04:40 -0400

    sql

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -188,7 +188,6 @@
 
 <!-- FOOTER -->
   <footer class="container">
-    <p class="float-right"><a href="#">Back to top</a></p>
     <p>&copy; 2019-2020 Mohawk College</p>
   </footer>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
