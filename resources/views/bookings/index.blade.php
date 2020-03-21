25fafe28f458e7ae32279dbd80b8b1cf4402480f
Developedbypatels <developedbypatels@gmail.com>
2020-03-20 20:58:43 -0400

    res

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -7,7 +7,7 @@
     <div class="form-group row">
         <label class="col-sm-2 col-form-label"for="room_number">Room Number</label>
         <div class="col-sm-10">
-            <input name="room_number" type="text" class="form-control" placeholder="Room Number" />
+            <input name="room_number" type="text" class="form-control" placeholder="Room Number" required />
             <small class="form-text text-muted">The room name being booked.</small>
         </div>
     </div>
@@ -15,7 +15,7 @@
     <div class="form-group row">
         <label class="col-sm-2 col-form-label"for="room_name">Room Name</label>
         <div class="col-sm-10">
-            <input name="room_name" type="text" class="form-control" placeholder="Room Name" />
+            <input name="room_name" type="text" class="form-control" placeholder="Room Name" required />
             <small class="form-text text-muted">The room name being booked.</small>
         </div>
     </div>
@@ -23,7 +23,7 @@
     <div class="form-group row">
         <label class="col-sm-2 col-form-label" for="guest_name">Guest Name</label>
         <div class="col-sm-10">
-            <input name="guest_name" type="text" class="form-control" placeholder="Guest Name" />
+            <input name="guest_name" type="text" class="form-control" placeholder="Guest Name" required />
             <small class="form-text text-muted">Guest name for the room being booked.</small>
         </div>
     </div>
@@ -31,7 +31,7 @@
     <div class="form-group row">
         <label class="col-sm-2 col-form-label" for="date">Date</label>
         <div class="col-sm-10">
-            <input name="date" type="date" class="form-control" placeholder="Date" />
+            <input name="date" type="date" class="form-control" placeholder="Date" required />
             <small class="form-text text-muted">Booking date for the room being booked.</small>
         </div>
     </div>
