786869aee7cad087e51af5f263dfaa002d487a7f
Developedbypatels <developedbypatels@gmail.com>
2020-03-21 23:49:28 -0400

    done

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<a class="btn btn-primary" href="{{ route('rooms.create') }}">Add New Room</a>
+<a class="btn btn-primary" href="{{ route('rooms.create') }}">Add New Room <i class="fa fa-plus" aria-hidden="true"></i></a>
 
 <table class="table">
     <thead>
