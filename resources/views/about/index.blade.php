756943f219b1da4c79b16f59e94856259ddfe6d6
Developedbypatels <developedbypatels@gmail.com>
2020-03-21 23:34:13 -0400

    done

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+<p><strong>Hello, Nice to Meet You&hellip;</strong></p>
+<p>Your About Us page is primarily an introduction, don&rsquo;t be so uptight! You should be having more fun on your About Us page. It should be written in a way that is conversational and introduces your company so a prospect can get to know, like and trust your brand. People buy from companies they know, like and trust and you only get one chance at a first impression. Don&rsquo;t blow this introduction, make sure you are memorable and likeable. Below are a few fun ways to turn your About Us Page into a marketing workhorse in a style that is fun and welcoming.</p>
+<p><strong>Talk to Your Visitors</strong></p>
+<p>We have already mentioned this tactic above but it so important we need to go into more detail. Write your About Us in a conversational tone. As people, we tend to write differently then we speak. Our writing is stuffier and more &ldquo;professional&rdquo; or &ldquo;academic&rdquo; than how we speak. On your About Us page, try to write in way that is more like you speak. This is your introduction to the website visitor, you don&rsquo;t need to impress them big words or corporate-speak. Just introduce yourself and your company like you would when someone asks you what you do for a living. When you answer that question in a real live conversation, I doubt you use terms like &ldquo;processes that decrease inefficiencies&rdquo; or &ldquo;innovative technologies.&rdquo; You simply let the person know what you do.</p>
+<p>Let&rsquo;s imagine you are an accountant. Which paragraph will be more appealing to someone who is visiting your site for the first time:</p>
+<blockquote>
+<p>We deliver a broad range of accounting, payroll and tax solutions, backed by technology, business analytics and consulting services that help create valuable relationships between our clients, their customers, employees and regulatory authorities.</p>
+</blockquote>
+<p>Or</p>
+<blockquote>
+<p>We stay on the cutting edge of tax law developments so that you don&rsquo;t have to. We do not believe in a set it and forget it system, our clients are our top priority. We are always striving to be better so our clients achieve more.</p>
+</blockquote>
+<p>I like the second one! By the time I am finished reading the first paragraph I am so bored, I am ready to click the back button and head off to Google to find a different company. The second paragraph, while shorter and easier to read, is also more conversational and the benefit to the prospect is more direct&ndash; they will be able to achieve more.</p>
+<p>The first paragraph tries what I call &ldquo;the shotgun approach to service.&rdquo; It states &ldquo;we do everything&rdquo; and the benefit is &ldquo;creating valuable relationships between our clients, their customers, employees and regulatory authorities.&rdquo; What the heck does that even mean? Don&rsquo;t get caught up in corporate-speak and jargon. Don&rsquo;t try to be everything to everyone, you end being nothing to anyone.</p>
+<p>Talk to your prospects like you are talking to a friend or business acquaintance. Treat them with respect by respecting their intelligence and they will be more likely to want to talk to you. This approach will get more prospective customers to know, like and trust you.</p>
+    </fieldset>
 </div>
+</div>
+
 @endsection
